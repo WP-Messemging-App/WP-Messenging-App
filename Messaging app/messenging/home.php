48f2e63d4cb7187dc2909e572bc0e4dc72713b38
surai-da <73590296+surai-da@users.noreply.github.com>
2020-10-29 00:44:44 +0700

    Update home.php

--- Messaging app/messenging/home.php
+++ Messaging app/messenging/home.php
@@ -3,6 +3,14 @@
 
 <head>
 	<title>Marsin</title>
+	<meta charset="utf-8">
+    <meta http-equiv="x-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googlepis.com/css?family=Roboto|Courgette|Pacifico:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script stc="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	
 	<link rel="stylesheet" type="text/css" href="css/home.css"> <!-- link for css home -->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
@@ -15,25 +23,30 @@
 				<form action="" class="form" id="form1" method="post">
 					<h2 class="form__title">Sign Up</h2>
 					<!-- This for Name -->
-					<input type="text" placeholder="Name" class="input" 
+					<input type="text" placeholder="Name" class="input" name="user_name"  autocomplete="on" required
 					pattern="[a-zA-Z]{5}" title="At lest 5 or more characters ,Try it without NUMBER">
+					
 					<!-- This for Last Name -->
-					<input type="text" placeholder="Last-Name" class="input" 
+					<input type="text" placeholder="Last-Name" class="input" name="user_lastname"  autocomplete="on" required
 					pattern="[a-zA-Z]{5}" title="At lest 5 or more characters ,Try it without NUMBER">
 					<!-- This for Email -->
-					<input type="email" placeholder="Email" class="input" 
+					<input type="email" placeholder="Email" class="input" name="user_email"  autocomplete="on" required
 					pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" title="Check it again">
 					<!-- This for Password -->
-					<input type="password" placeholder="Password" class="input" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-					 title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters">
-					 
-					 <input type="date" placeholder="Birthday" class="input" id="birthday" name="birthday">
-					 
-					 <select name="gender" id="gender"class="input">
+					<input type="password" placeholder="Password" class="input" name="user_password"  autocomplete="on" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+				    title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters">
+					<!-- This for choose date --> 
+				    <input type="date" placeholder="Birthday" class="input" id="birthday" name="user_birthday">
+					<!-- This for choose gender -->
+					<select name="user_gender" id="gender"class="input">
+						<option disabled="">Select your Gender</option>
 						<option value="male">Male</option>
 						<option value="female">Female</option>
-					  </select>
-					<button class="btn">Register</button>
+					</select>
+					<!-- This button -->
+					<button type="submit" class="btn" name="sign_up">Register</button>
+					<!-- <?php //include("signup_user.php");?> -->
+					<!-- <div class="text-center small" style="color: #67428B;">Already have accound? <a href="signup.php">Create one</a></div> -->
 				</form>
 			</div>
 
@@ -48,7 +61,7 @@
 					<a href="forgot_pass.php" class="link">Forgot your password?</a>
 					<button type="submit" class="btn" name="sign_in" >Log-In</button>
 					<!-- <?php //include("signin_user.php");?> -->
-					<div class="text-center small" style="color: #67428B;">Don't have an account? <a href="signup.php">Create one</a></div>
+					<!-- <div class="text-center small" style="color: #67428B;">Don't have an account? <a href="signin.php">Sign in here</a></div> -->
 				</form>
 			</div>
 
